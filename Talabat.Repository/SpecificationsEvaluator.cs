5efd57b0e7d49fa1113baaa93ffe016d21c81fd5
Mahmoud Salah <elshokhaby1999@gmail.com>
2024-10-06 14:14:52 +0300

    Apply Specification Design Pattern

---
+++ Talabat.Repository/SpecificationsEvaluator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Specifications;
+
+namespace Talabat.Repository
+{
+    internal static class SpecificationsEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        //This function has all responsibility for generate  dynamic queries...
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecifications<TEntity> spec)
+        {
+            var query = inputQuery; // _dbContext.Set(TEntity)
+           
+            if(spec.Criteria is not null)
+               return query = query.Where(spec.Criteria); // _dbContext.Set(TEntity).Where("Criteria") : "Criteria" >> (P => P.Id == 1).
+
+            //Includes: may be the query has many includes for example : product has
+            //1.Include(P => P.Brand)
+            //2.Include(P => P.Category)
+            //..So on..
+            //So We Use Aggregate Linq Operator to acumlate
+
+            query = spec.Includes.Aggregate(query, (currentQuery, includeExpression) => currentQuery.Include(includeExpression));
+            
+            return query;
+        }
+    }
+}
