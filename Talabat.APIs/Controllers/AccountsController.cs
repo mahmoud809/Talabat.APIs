2437438537c774ba27f08eef2c8dc4bdaad82a21
Mahmoud Salah <elshokhaby1999@gmail.com>
2024-10-17 19:48:42 +0300

    Register EndPoint

--- Talabat.APIs/Controllers/AccountsController.cs
+++ Talabat.APIs/Controllers/AccountsController.cs
@@ -35,5 +35,27 @@ namespace Talabat.APIs.Controllers
             });
 
         }
+
+        [HttpPost("register")] //POST : /api/accounts/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+        {
+            var user = new AppUser()
+            {
+                DisplayName = model.DisplayName,
+                Email = model.Email, //mahmoud.salah@gmail.com
+                UserName = model.Email.Split('@')[0], //mahmoud.salah
+                PhoneNumber = model.PhoneNumber
+            };
+
+            var result = await _userManager.CreateAsync(user);
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+            return Ok(new UserDto()
+            {
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                Token = "This token will be generated soon..!"
+            });
+            
+        }
     }
 }
